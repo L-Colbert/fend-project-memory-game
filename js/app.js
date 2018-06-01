1305fba518f0ec8d46ebb4e62a9258b94a369d80
Libby Colbert <colbert.libby@gmail.com>
2018-06-01 18:28:35 -0400

    Update comments

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
     /*
     * Create a list that holds all of your cards
     */
+    
     const cardsArray = [ 
         'fa fa-diamond', 'fa fa-diamond',
         'fa fa-leaf', 'fa fa-leaf',
@@ -19,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
         'fa fa-cube', 'fa fa-cube',
         'fa fa-bicycle', 'fa fa-bicycle'];
 
+    
     /*
     * Display the cards on the page
     *   - shuffle the list of cards using the provided 'shuffle' method below
@@ -42,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
         return array;
     }
 
-
+    //displays the cards on the deck
     function displayCards(array) {
         const deck = document.querySelector('.deck');
         const cardList = document.querySelectorAll('li.card');
@@ -69,10 +71,9 @@ document.addEventListener('DOMContentLoaded', function() {
     *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
     *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
     *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-    *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
     */
 
- 
+    //Main game functionality
     function playGame(e, count){
         const newCard = e.target;
 
@@ -84,19 +85,19 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         };
 
-        //creates an array of clicked(open) cards
+        //add the card to a *list* of 'open' cards
         function openedCards(e, card){
             cardHolder.push(card);
             return cardHolder;
         };
 
-        //display messages/instructions when game is won
+        //if all cards have matched, display a message with the final score
         function gameWon(){
             alert('Congratulations! You Did it!!');
         }
         
         
-        //locks cards open if they are a match
+        //if the cards do match, lock the cards in the open position
         function matched() {
             
             function setAttr(item) {
@@ -116,7 +117,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 
         };
 
-        //locks cards open if they are a match
+        //if the cards do not match, remove the cards from the list and hide the card's symbol
         function unmatched() {
             function setAttr(item) {
                 item.classList.remove('open', 'show');
@@ -134,7 +135,7 @@ document.addEventListener('DOMContentLoaded', function() {
         //invokes function that stores the clicked in an array
         const cardArray = openedCards(e, newCard);
 
-        //check to see if cards match
+        //if the list already has another card, check to see if the two cards match
         if (cardArray.length === 2) {
             if (cardArray[0].firstElementChild.getAttribute('class') === cardArray[1].firstElementChild.getAttribute('class')) {
                 matched();
@@ -177,7 +178,6 @@ document.addEventListener('DOMContentLoaded', function() {
         count = 0;
         document.querySelector('.timer').innerHTML = '00:00:00';
         document.querySelector('.moves').textContent = count;
-   //     document.querySelector('.paused').style.visibility = 'hidden';
         document.querySelector('.paused button').innerHTML = 'Restart';
 
         
@@ -185,36 +185,25 @@ document.addEventListener('DOMContentLoaded', function() {
         const deck = document.querySelector('.deck');
         const cardList = document.querySelectorAll('li.card');
         const imgList = document.querySelectorAll('li.card > i');
-        console.log(cardList);
-        console.log(document.querySelectorAll('li.card > i'));
         for(let i = 0; i < cardList.length; i++) {
             cardList[i].classList.remove('match');
             cardList[i].classList.remove('open', 'show');
             cardList[i].removeChild(imgList[i]);           
         };
-
-    
-
-        // while (deck.firstChild) {
-        //     deck.removeChild(deck.firstChild);
-
         displayCards(shuffle(cardsArray))
-        
-
     };
-
 
     function startStop() {
     if(running === true) {
         increment();
         document.querySelector('.paused').style.visibility = 'visible';
+        document.querySelector('.restart').style.visibility = 'visible';
         document.querySelector('.paused button').innerHTML = 'Pause';
         } else {
             document.querySelector('.paused button').innerHTML = 'Resume';
             increment();
             }
     };
-
 
     //shuffle cards and display
     const newArray = shuffle(cardsArray);
@@ -232,12 +221,20 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                 count++;
                 //updates the moves counter on each card clicked
-                document.querySelector('.moves').textContent = count;
+                document.querySelector('.moves').textContent = 'Moves: '+ count;
                 playGame(event, count); 
             };   
         };
     }
-        
+
+    //increment the move counter and display it on the page
+
+
+    /*
+    * set up the event listener for a card. If a card is clicked:
+    */
+    
+
     //starts the game only if the card(li) is clicked
     document.querySelector('.deck').addEventListener('click', function() {
         ifTarget(event);
